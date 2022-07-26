e23debcdfbda621fdfed61f1d943829ebad4cbc6
Heitor Assis <heitor.assis@eduzz.com>
2022-07-25 21:33:44 -0300

    feat: adiciona integracao com authorizer no core

--- packages/core/start/routes.ts
+++ packages/core/start/routes.ts
@@ -1,38 +1,28 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer''
-|
-*/
-
+import Env from '@ioc:Adonis/Core/Env'
 import Route from '@ioc:Adonis/Core/Route'
 
+let authorizationMiddleware = 'auth'
+const useKeycloak = Env.get('USE_KEYCLOAK')
+
+if (useKeycloak) {
+  authorizationMiddleware = 'keycloak'
+}
+
 Route.resource('questions', 'QuestionsController').apiOnly().middleware({
-  store: 'auth',
-  update: 'auth',
-  destroy: 'auth',
+  store: authorizationMiddleware,
+  update: authorizationMiddleware,
+  destroy: authorizationMiddleware,
 })
 
 Route.resource('questions.answers', 'AnswersController').apiOnly().middleware({
-  store: 'auth',
-  update: 'auth',
-  destroy: 'auth',
+  store: authorizationMiddleware,
+  update: authorizationMiddleware,
+  destroy: authorizationMiddleware,
 })
 
 Route.resource('registration', 'RegistrationsController').only(['store'])
 
-Route.post('auth/login', 'AuthController.login')
-Route.get('auth/logout', 'AuthController.logout').middleware('auth')
+if (!useKeycloak) {
+  Route.post('auth/login', 'AuthController.login')
+  Route.get('auth/logout', 'AuthController.logout').middleware('auth')
+}
