1d57c58671890f81118a88667b56e9ce16cf22e9
Heitor Assis <heitor.assis@eduzz.com>
2022-10-03 20:05:40 -0300

    feat: alteracoes no questions

--- packages/questions/app/Listeners/Question.ts
+++ packages/questions/app/Listeners/Question.ts
@@ -1,5 +1,27 @@
 import { EventsList } from '@ioc:Adonis/Core/Event'
 
-export default class Answer {
-  public async onNewQuestion(questionModel: EventsList['new:question']) {}
+import AmqpListener from './AmqpListener'
+
+export default class Question extends AmqpListener {
+  public async onNewQuestion(questionModel: EventsList['new:question']) {
+    await questionModel.load('author')
+
+    const question = questionModel.toJSON()
+
+    this.subscribeAuthor(question, question.author)
+  }
+
+  private subscribeAuthor(question: Record<string, unknown>, author: Record<string, unknown>) {
+    const messageContent = {
+      question: {
+        id: question.id,
+      },
+      subscriber: {
+        id: author.id,
+        email: author.email,
+      },
+    }
+
+    this.publishMessage('new:question', messageContent)
+  }
 }
